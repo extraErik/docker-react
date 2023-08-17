1fe69ff2997943604b68b561febafe437f1ebe34
Erik Rygg <erygg01@live.com>
2023-08-17 12:02:34 -0500

    try alternate fix for docker login and test html change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Actually not weird, you just need to pay attention.
+          Here it is.
         </p>
         <a
           className="App-link"
